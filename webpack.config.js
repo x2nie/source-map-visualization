9fa3f892d60b952d9d29e4f358f6d496e18a3527
Fathony L <fathony@smart-leaders.net>
2026-04-02 12:47:48 +0700

    cleanup, del dead files

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.json$/, loader: "json-loader" },
-			{ test: /\.jade$/, loader: "jade-loader" },
 			{ test: /\.css$/,  loader: "style-loader!css-loader" },
 			{ test: /\.less$/,  loader: "style-loader!css-loader!less-loader" },
 			{ test: /\.png$/,  loader: "url-loader?limit=5000&minetype=image/png" }
